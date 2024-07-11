ef21911cb249670f0cbaa6ee51a61159fefdeec6
Mahmoud AbouElawad <35901393+abouelawad@users.noreply.github.com>
2024-07-11 09:18:14 +0300

    edit route group

---
+++ routes/dashboard.php
@@ -0,0 +1,21 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
+
+
+
+
+Route::group([
+  'prefix'=>'categories',
+  'as'    =>'categories.',
+],function(){
+  Route::get('/', [CategoryController::class, 'index'])->name('name');
+  Route::get('/create', [CategoryController::class, 'create'])->name('create');
+  Route::get('/{category}', [CategoryController::class, 'show'])->name('show');
+  Route::post('/' , [CategoryController::class, 'store'])->name('store');
+  Route::get('/edit/{category}', [CategoryController::class, 'edit'])->name('edit');
+  Route::put('/{category}', [CategoryController::class, 'update'])->name('update');
+  Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('destroy');
+});
--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,13 @@ Route::get('/', function () {
     
     return view('welcome');
 });
-Route::prefix('/categories')->as('categories.')->group(function(){
 
-    Route::get('/', [CategoryController::class, 'index'])->name('name');
-    Route::get('/create', [CategoryController::class, 'create'])->name('create');
-    Route::get('/{category}', [CategoryController::class, 'show'])->name('show');
-    Route::post('/' , [CategoryController::class, 'store'])->name('store');
-    Route::get('/edit/{category}', [CategoryController::class, 'edit'])->name('edit');
-    Route::put('/{category}', [CategoryController::class, 'update'])->name('update');
-    Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('destroy');
 
-});
+require __DIR__.'/dashboard.php';
 
 
 
 
-Route::get('/dashboard', function(){
-    return view('layouts.dashboard');
-});
+
+
+
